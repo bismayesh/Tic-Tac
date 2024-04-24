59e116194a228d9dbd734ded48910d4f6266cb72
bismayesh <125033480+bismayesh@users.noreply.github.com>
2024-04-24 10:53:12 +0200

    character movement

--- Assets/Supercyan Character Pack Free Sample/Scripts/CharController.cs
+++ Assets/Supercyan Character Pack Free Sample/Scripts/CharController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class CharController : MonoBehaviour
 {
+
+    private Rigidbody rb;
+    private bool walkingRight = true;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Switch();
+        }
+    }
+
+    private void Switch()
+    {
+        walkingRight = !walkingRight;
+
+        if (walkingRight)
+            transform.rotation = Quaternion.Euler(0, -45, 0); // Change from 45 to -45 for turning left
+        else
+            transform.rotation = Quaternion.Euler(0, 45, 0); // Change from -45 to 45 for turning right
     }
 }
